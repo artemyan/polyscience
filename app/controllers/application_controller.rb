b6098f83aa21bac1d857b68a4c71bac8617afa38
justvitalius <justvitalius@gmail.com>
2016-04-04 11:37:10 +0300

    Add styles for images inside the article.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_default_meta_tags,
-                :set_public_locale
+  before_action :set_public_locale,
+                :set_default_meta_tags
+
 
   def access_denied(exception)
     redirect_to '/'
